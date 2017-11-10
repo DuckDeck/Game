8a417554ad084ccfa641d22e0b9ff6d8c98b5285
“stanhu” <3421902@qq.com>
2017-11-10 17:52:28 +0800

    add animtaion script

---
+++ Rice Attack/Assets/Scripts/Player/PlayerAnimtaion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimtaion : MonoBehaviour {
+
+	public Animator[] animators;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Rice Attack/Assets/Scripts/Scenes0/LoginManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoginManager : MonoBehaviour {
+
+	public GameObject operation;
+
+
+	public void onClickPlay1(){
+		operation.SetActive (true);
+	}
+
+	public void onClickPlay2(){
+		SceneManager.LoadScene (1);
+	}
+
+
+	public void PlayAudio(){
+		SoundManager.Instance.PlayAudioByName ("bombExplosion");
+	}
+
+
+	public void PlayBgMusic(){
+		SoundManager.Instance.PlayMusicByName ("background");
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
